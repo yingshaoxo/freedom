a66aeb5679f77970dbbe37de18038416b7986236
yingshaoxo <yingshaoxo@gmail.com>
2020-04-16 21:25:52 +0800

    data is ready

--- server/main.py
+++ server/main.py
@@ -1,12 +1,12 @@
-import everyday_pb2
 from sanic import Sanic, response
 from sanic_cors import CORS
 import datetime
 import pprint
+
 import base64
+import everyday_pb2
 
 from PIL import Image
-import cv2
 import io
 
 
@@ -61,6 +61,32 @@ async def user_management(request):
                 printit(everyday)
 
                 result["status"] = "ok"
+    return response.json(result)
+
+@app.route('/api/v1/upload', methods=['POST'])
+async def user_management(request):
+    result = {"status": "wrong"}
+    if request.json:
+        action = request.json.get("action")
+        data = request.json.get("data")
+        if action and data:
+            printit(data[:30])  # raw data
+            data = base64.b64decode(data)  # encoded by our self with our own javascript function
+            printit(data[:30])
+            data = base64.decodebytes(data)  # encoded by google protocol
+            printit(data[:30])  # mergeable bytes data for protocol object
+            oneday = everyday_pb2.OneDay()
+            oneday.MergeFromString(data)
+            printit(oneday.content[0].text)
+
+            base64_image_string = oneday.content[0].image[0]
+            printit(base64_image_string[:30])
+            show_image(base64_image_string)
+
+            everyday.oneday.extend([oneday])
+            printit(everyday)
+
+            result["status"] = "ok"
     return response.json(result)
 
 
