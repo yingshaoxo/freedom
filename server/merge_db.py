1fac259a5d6f1f150ee9f23ea988a5cb880236f0
yingshaoxo <yingshaoxo@gmail.com>
2020-04-29 08:43:18 +0800

    now we support sqlite db merging

---
+++ server/merge_db.py
@@ -0,0 +1,31 @@
+import os
+import sqlite3
+
+
+class Self():
+    _root_path = "."
+    pass
+
+
+self = Self()
+self.SQL_DATA_FILE = os.path.join(self._root_path, "qzone.db")
+self._sql_conn = sqlite3.connect(self.SQL_DATA_FILE, check_same_thread=False)
+
+self._sql_cursor = self._sql_conn.cursor()
+self._sql_cursor.execute('''CREATE TABLE IF NOT EXISTS thoughts
+            (date TEXT, type TEXT, text TEXT, images TEXT)''')
+
+
+SQL_DATA_FILE = os.path.join(".", "yourdata.db")
+_sql_conn = sqlite3.connect(SQL_DATA_FILE, check_same_thread=False)
+
+_sql_cursor = _sql_conn.cursor()
+_sql_cursor.execute('''CREATE TABLE IF NOT EXISTS thoughts
+            (date TEXT, type TEXT, text TEXT, images TEXT)''')
+
+
+for row in self._sql_cursor.execute('SELECT * FROM thoughts ORDER BY date'):
+    _sql_cursor.execute("INSERT INTO thoughts VALUES (?,?,?,?)",
+                        row
+                        )
+    _sql_conn.commit()
