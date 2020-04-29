058bb4ad46bbbafd5cca9ceab658ad3dd3964361
yingshaoxo <yingshaoxo@gmail.com>
2020-04-30 06:25:08 +0800

    add fetch method for kivydiary and mine; next one is twitter

--- server/merge_db.py
+++ server/merge_db.py
@@ -1,4 +1,12 @@
-from data import Data
+import os
 
+from data import Data
 my_data = Data(".")
-my_data._new_data.merge_from_a_database("qzone.db")
+
+# Get the list of all files in directory tree at given path
+list_of_files = list()
+for (dirpath, dirnames, filenames) in os.walk(os.path.abspath("../fetch/")):
+    list_of_files += [os.path.join(dirpath, file) for file in filenames if file[-3:] == ".db"]
+
+for file in list_of_files:
+    my_data._new_data.merge_from_a_database(file)
