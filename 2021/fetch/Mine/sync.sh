955c495f1fe3025ab4386c396281bc94e55c07cb
yingshaoxo <yingshaoxo@gmail.com>
2025-08-03 22:15:57 +0800

    update diary

--- 2021/fetch/Mine/sync.sh
+++ 2021/fetch/Mine/sync.sh
@@ -2,8 +2,9 @@ python3 convert_to_txt.py
 
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/CS/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
 
-cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/CS/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
 
+cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Code/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
+cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Code_Mini/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
 
 
 cp ./database.json /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Code/freedom/2021/fetch/Mine/database.json
@@ -12,7 +13,6 @@ cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingsh
 
 
 cp ./database.json /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Diary/database.json
-
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Diary/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
 
 
