05cf2f3e63896aab68f7e36ae7217b28571eb6ed
yingshaoxo <yingshaoxo@gmail.com>
2025-10-25 07:42:41 +0800

    add date info for diary

--- 2021/fetch/Mine/convert_to_txt.py
+++ 2021/fetch/Mine/convert_to_txt.py
@@ -6,13 +6,18 @@ with open("database.json", "r") as f:
 json_object = json.loads(text)
 json_object[0]
 
+date_list = []
 new_list = []
 for one in json_object:
     if one["type"] != "qzone":
         new_list.append(one["content"])
+        date_list.append(one["date"])
 
 seperator = "\n\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n\n"
 new_list = [one.replace("​", "") for one in new_list]
 new_text = seperator.join(new_list)
 with open("all_yingshaoxo_data_2023_11_13.txt", "w", encoding="utf-8", errors="ignore") as f:
     f.write(new_text)
+
+with open("date_for_all_yingshaoxo_data_2023_11_13.txt", "w", encoding="utf-8", errors="ignore") as f:
+    f.write("\n".join(date_list))
--- 2021/fetch/Mine/sync.sh
+++ 2021/fetch/Mine/sync.sh
@@ -1,6 +1,7 @@
 python3 convert_to_txt.py
 
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/CS/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
+cp ./date_for_all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/CS/yingshaoxo_txt_data/date_for_all_yingshaoxo_data_2023_11_13.txt
 
 
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Code/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
