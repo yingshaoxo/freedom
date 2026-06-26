0180f21b99f937d6df9aad03e2e550737bdf0ee7
yingshaoxo <yingshaoxo@gmail.com>
2026-06-26 10:57:36 +0800

    let your grandfather and father be stronger, then you will not be weak.

--- 2021/fetch/Mine/sync.sh
+++ 2021/fetch/Mine/sync.sh
@@ -12,10 +12,8 @@ cp ./database.json /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Code/freedom/2021/fetch/Mine/all_yingshaoxo_data_2023_11_13.txt
 
 
-
 cp ./database.json /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Diary/database.json
 cp ./all_yingshaoxo_data_2023_11_13.txt /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Diary/yingshaoxo_txt_data/all_yingshaoxo_data_2023_11_13.txt
-
 
 
 cp ./database.json /home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Core/Small_Core/My_Diary/freedom/2021/fetch/Mine/database.json
